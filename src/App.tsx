cef020577533d65cedd1376eec69977dd29ee329
Renan Romagnoli <renanromagnoli@yahoo.com.br>
2022-08-24 04:15:32 +0000

    html created

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,22 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+   <div className='container'>
+       <div className="logo">
+           <img src="https://www.tailorbrands.com/wp-content/uploads/2021/01/nike-logo.png" alt="Super Shoes"/>
+       </div>
+       <div className="carousel">
+           <div className="item">
+               <div className="image">
+                   <img src="https://imgnike-a.akamaihd.net/1300x1300/013635ID.jpg" alt="shoe"/>
+               </div>
+               <div className="info">
+                   <span className="name">Super Shoe 1</span>
+                   <span className="oldPrice">U$ 299.00</span>
+                   <span className="price">U$ 199.00</span>
+               </div>
+           </div>
+       </div>
+   </div>
   );
 }
